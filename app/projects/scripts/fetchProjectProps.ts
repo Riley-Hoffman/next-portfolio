99be3a683ea497602c1f4091bc833d74dea9a183
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-09-06 09:13:52 -0400

    Fix folder structure for netlify functions

--- app/projects/scripts/fetchProjectProps.ts
+++ app/projects/scripts/fetchProjectProps.ts
@@ -1,5 +1,5 @@
 export async function fetchProjectProps() {
-    const response = await fetch('http://localhost:8888/.netlify/functions/projects-mysql');
+    const response = await fetch('https://localhost:8888/.netlify/functions/projects-mysql');
     
     if (!response.ok) {
       console.error('Failed to fetch projects:', response.statusText);
@@ -9,7 +9,6 @@ export async function fetchProjectProps() {
     }
   
     const projects = await response.json();
-    console.log('fetchProjectProps', projects)
   
     return {
       props: {
