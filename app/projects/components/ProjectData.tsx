fe7be3aa991f2aaef5edf7975dd933677f089baf
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-09-18 08:39:26 -0400

    Moved project images to projects folder

--- app/projects/components/ProjectData.tsx
+++ app/projects/components/ProjectData.tsx
@@ -1,9 +1,9 @@
-import evangeline from '../../../images/evangeline-gentle-music-project.webp';
-import particleCleanup from '../../../images/particle-cleanup-project.webp';
-import outOfContext from '../../../images/out-of-context-project.webp';
-import infinityCorkboard from '../../../images/infinity-corkboard-project.webp';
-import weatherScout from '../../../images/weather-scout-project.webp';
-import delicious from '../../../images/delicious-project.webp';
+import evangeline from '../images/evangeline-gentle-music-project.webp';
+import particleCleanup from '../images/particle-cleanup-project.webp';
+import outOfContext from '../images/out-of-context-project.webp';
+import infinityCorkboard from '../images/infinity-corkboard-project.webp';
+import weatherScout from '../images/weather-scout-project.webp';
+import delicious from '../images/delicious-project.webp';
 
 export interface Project {
     title: string;
