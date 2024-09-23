26f498d95daf23fa0c3343302c1313c4ab857bd3
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-09-23 15:46:35 -0400

    Skills, use gap-x/gap-y on grid instead of p-/m- on children.

--- app/skills/components/SkillItem.tsx
+++ app/skills/components/SkillItem.tsx
@@ -12,8 +12,8 @@ interface SkillItemProps {
 
 export const SkillItem: React.FC<SkillItemProps> = ({ skill, icon, devicon, image }) => {
   return (
-    <li className="my-8 text-center">
-      <div className="px-3 flex justify-center items-center min-h-[2.766rem] skill-icon-box">
+    <li className="text-center">
+      <div className="flex justify-center items-center min-h-[2.766rem] skill-icon-box">
         {devicon ? (
           <i className={`text-5xl ${devicon}`} aria-hidden="true"></i>
         ) : image ? (
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -49,7 +49,7 @@ export default function Skills()  {
       <section className="pb-16">
         <h1 className="text-center text-3xl leading-normal bg-[#eee2f3] border-b-2 mb-7 mt-0 py-10 px-5 gradient-border inverted md:text-5xl md:leading-normal contrast-more:bg-white" aria-live="polite">Skills</h1>
         <div className="max-w-screen-xl relative">
-            <ul className="max-w-5xl pt-5 pb-12 mt-16 grid grid-cols-2 justify-items-center skills-list sm:grid-cols-3 md:grid-cols-4" aria-label="Skills">
+            <ul className="max-w-5xl pt-9 pb-12 mt-16 grid grid-cols-2 gap-y-16 gap-x-6 justify-items-center skills-list sm:grid-cols-3 md:grid-cols-4" aria-label="Skills">
               {skills.map((skill, index) => (
                 <SkillItem key={index} skill={skill.skill} devicon={skill.devicon} icon={skill.icon} image={skill.image} />
               ))}
