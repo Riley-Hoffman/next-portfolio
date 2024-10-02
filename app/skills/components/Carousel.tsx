a2d6d499ef237e588b95b11eebdec09ee24e8d90
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-02 10:08:46 -0400

    customer-swiper.css: changed prev next border color

--- app/skills/components/Carousel.tsx
+++ app/skills/components/Carousel.tsx
@@ -19,7 +19,7 @@ interface CarouselProps {
 }
 
 export const Carousel = ({ slides }: CarouselProps) => {
-  const commonClasses = 'absolute top-1/2 transform -translate-y-1/2 bg-pink-100 rounded-full py-1 px-4 shadow-lg border border-purple-100 z-10 hover:bg-offwhite hover:border-zinc focus-visible:bg-offwhite focus-visible:border-zinc';
+  const commonClasses = 'absolute top-1/2 transform -translate-y-1/2 bg-pink-100 rounded-full py-1 px-4 shadow-lg border border-[#C2B9FF] z-10 hover:bg-offwhite hover:border-zinc focus-visible:bg-offwhite focus-visible:border-zinc';
 
   return (
     <div className="relative max-w-[700px] overflow-hidden" role="listbox" aria-label="Trainings & Certifications">
