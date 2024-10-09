a5b68adc2b2bdb112281b2a1dc74c9e00fc82b3b
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-09 11:53:13 -0400

    Projects: moved ProjectImages to own component

--- app/projects/components/ProjectsList.tsx
+++ app/projects/components/ProjectsList.tsx
@@ -3,12 +3,7 @@ import { ProjectBox } from './ProjectBox';
 import { ProjectContent } from './ProjectContent';
 import { useFetchFirebaseData } from '../../../hooks/useFetchFirebaseData';
 import type { Project } from './ProjectContent';
-import evangeline from '../images/evangeline-gentle-music-project.webp';
-import particleCleanup from '../images/particle-cleanup-project.webp';
-import outOfContext from '../images/out-of-context-project.webp';
-import infinityCorkboard from '../images/infinity-corkboard-project.webp';
-import weatherScout from '../images/weather-scout-project.webp';
-import delicious from '../images/delicious-project.webp';
+import { ProjectImages } from './ProjectImages';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -37,22 +32,13 @@ export default function ProjectsList() {
 function ProjectCategory({ category, index, projects }: { category: string; index: number; projects: Project[] }) {
   const filteredProjects = projects.filter((p: Project) => p.category === category);
 
-  const imageMap: { [key: string]: string } = {
-    'Evangeline Gentle Music': evangeline.src,
-    'Particle Cleanup Game': particleCleanup.src,
-    'Out Of Context': outOfContext.src,
-    'Infinity Corkboard': infinityCorkboard.src,
-    'Weather Scout': weatherScout.src,
-    'Delicious': delicious.src,
-  };
-
   return (
     <>
       <h2 className="max-w-screen-xl text-2xl pb-5 leading-normal md:text-3xl">{category} Projects</h2>
       <ul className="max-w-screen-xl pb-5 text-base" aria-label={`${category} Projects`}>
         {filteredProjects.map((project: Project, idx: number) => (
           <ProjectBox key={project.title} inverted={idx % 2 !== 0 ? 'inverted' : ''} animation={project.animation} isFirst={index === 0 && idx === 0}>
-            <ProjectContent {...project} imgUrl={imageMap[project.title]} isFirst={index === 0 && idx === 0} />
+            <ProjectContent {...project} imgUrl={ProjectImages[project.title]} isFirst={index === 0 && idx === 0} />
           </ProjectBox>
         ))}
       </ul>
