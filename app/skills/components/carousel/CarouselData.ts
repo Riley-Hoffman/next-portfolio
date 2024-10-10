03cb508539258ecc8dc312dd5d5bc2f2d527b8bc
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-10 17:22:32 -0400

    CarouselData: added node.js training certificate

--- app/skills/components/carousel/CarouselData.ts
+++ app/skills/components/carousel/CarouselData.ts
@@ -1,8 +1,10 @@
+import node1 from './images/node1.webp'
 import aws1 from './images/aws1.webp'
 import drupal1 from './images/drupal1.webp'
 import drupal2 from './images/drupal2.webp'
 
 export const CarouselData = [
+  {src: node1, label: 'Node.js Essential Training'},
   {src: aws1, label: 'AWS Essential Training for Developers'},
   {src: drupal1, label: 'Drupal 9 Essential Training 1: Introduction and Administration'},
   {src: drupal2, label: 'Drupal 9 Essential Training 2: Content and Fields'},
