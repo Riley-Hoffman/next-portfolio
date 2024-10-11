81304d704bccb24243705fe7f34edba58c39c30b
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-11 11:03:14 -0400

    ProjectContent: refactored rendering logic to be dry

--- app/projects/components/ProjectContent.tsx
+++ app/projects/components/ProjectContent.tsx
@@ -18,9 +18,29 @@ export interface Project {
 
 export function ProjectContent({ title, skills, description, internal, liveUrl, gitUrl, alt, imgUrl, isFirst }: Project) {
   const commonClasses = "mx-5 py-1 px-3 uppercase button";
+  
+  const renderLink = (url: string, content: JSX.Element, isInternal?: boolean) => (
+    isInternal ? (
+      <Link className={commonClasses} href={url}>
+        {content}
+      </Link>
+    ) : (
+      <a className={`new-tab ${commonClasses}`} href={url} target="_blank" rel="noopener noreferrer">
+        {content}
+        <NewTabSrText icon={true} />
+      </a>
+    )
+  );
+
   const liveContent = (
     <>
       <span className="sr-only">{title} </span>Live
+    </>
+  );
+
+  const repoContent = (
+    <>
+      <span className="sr-only">{title} </span>Repo
     </>
   );
 
@@ -28,29 +48,13 @@ export function ProjectContent({ title, skills, description, internal, liveUrl, 
     <>
       <div className="pt-5 pb-10 basis-2/4 project-info">
         <h2 className="pb-4">{title}</h2>
-        <p className="pb-4 mb-8 text-lg font-poppins" translate="no">
-          {skills}
-        </p>
+        <p className="pb-4 mb-8 text-lg font-poppins" translate="no">{skills}</p>
         <p className="pb-4">{description}</p>
         <h3 className="mb-8">
           <span translate="no">{title} </span>Links:
         </h3>
-        {internal ? (
-          <Link className={`${commonClasses}`} href={liveUrl}>
-            {liveContent}
-          </Link>
-        ) : (
-          <a className={`new-tab ${commonClasses}`} href={liveUrl} target="_blank" rel="noopener noreferrer" >
-            {liveContent}
-            <NewTabSrText icon={true} />
-          </a>
-        )}
-        {gitUrl && (
-          <a className={`ml-3 ${commonClasses} new-tab`} href={gitUrl} target="_blank" rel="noopener noreferrer" >
-            <span className="sr-only">{title} </span>Repo
-            <NewTabSrText icon={true} />
-          </a>
-        )}
+        {renderLink(liveUrl, liveContent, internal)}
+        {gitUrl && renderLink(gitUrl, repoContent)}
       </div>
       <div className="text-center relative basis-2/4">
         <Image className="max-w-full w-[35.563rem]" alt={alt} src={imgUrl} title={title} height="569" width="569" priority={isFirst} />
