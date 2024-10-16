145649503adc795982a9b148dd0c6778cd9286c8
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-16 09:50:31 -0400

    layout.tsx fixed indentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,7 @@ export const metadata: Metadata = {
 }
 
 const DynamicImports = dynamic(() => import('./components/DynamicImports'));
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+  export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
