584ba5350217a7fcf72581d8aad9e28377006342
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-18 10:06:28 -0400

    postcss.config.js: added csssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(isProduction ? {
+      cssnano: {
+        preset: 'default',
+      },
+    } : {})
   },
-}
+};
