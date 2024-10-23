3da113771212cfd21296bb0345ebf2db0140d50c
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-23 11:31:06 -0400

    FirstFoldContent: added rounded-sm to text box

--- app/components/home/FirstFoldContent.tsx
+++ app/components/home/FirstFoldContent.tsx
@@ -2,7 +2,7 @@ export const FirstFoldContent = () =>  (
     <>
         <h1 className="font-semibold m-0 text-3xl leading-normal drop-shadow-[1px_1px_0_#f5eef7] md:text-4xl md:leading-normal">Riley Hoffman</h1>
         <p className="mt-0 relative after:absolute after:bottom-[-0.75rem] after:left-5 after:bg-purple-200 after:h-3 after:w-5"><span className="inline-block w-40 m-o mr-auto border-r-2 border-purple-200 border-solid whitespace-nowrap overflow-hidden tracking-widest font-medium drop-shadow-[1px_1px_0_#f5eef7] motion-safe:animate-typetext">Web Developer</span></p>
-        <p className="mt-8 mx-5 px-[0.625rem] py-2 font-medium bg-[#f5eef6a3] border border-zinc">I am a web developer with a passion for building accessible and responsive web applications. I quickly learn new concepts and love adding to my growing skill set. I am a proactive problem solver who enjoys writing future-proof, understandable code that fosters collaboration with other developers.</p>
+        <p className="mt-8 mx-5 px-[0.625rem] py-2 font-medium bg-[#f5eef6a3] border border-zinc rounded-sm">I am a web developer with a passion for building accessible and responsive web applications. I quickly learn new concepts and love adding to my growing skill set. I am a proactive problem solver who enjoys writing future-proof, understandable code that fosters collaboration with other developers.</p>
     </>
 )
 
