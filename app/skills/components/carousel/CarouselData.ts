73ba462351968233cebd2afec6fa98d5c81c4ad2
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-24 13:10:23 -0400

    CarouselData: added elasticsearch essential tranining certificate

--- app/skills/components/carousel/CarouselData.ts
+++ app/skills/components/carousel/CarouselData.ts
@@ -1,4 +1,5 @@
 export const CarouselData = [
+  {src: 'https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/elasticsearch1.webp', label: 'Elasticsearch Essential Training'},
   {src: 'https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/restapinodeexpress.webp', label: 'Building RESTful APIs with Node.js and Express'},
   {src: 'https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/node1.webp', label: 'Node.js Essential Training'},
   {src: 'https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/aws1.webp', label: 'AWS Essential Training for Developers'},
