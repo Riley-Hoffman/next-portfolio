11afe30f8de1ce9bf9b4b3b1fdde9794efe4c41d
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-25 09:20:03 -0400

    CoverImage: Used html img for srcset since Next/image is unoptimized.

--- app/components/home/CoverImage.tsx
+++ app/components/home/CoverImage.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { useRef } from 'react';
 
@@ -10,13 +9,15 @@ interface CoverImageProps {
     width: number,
     height: number,
     srcImg: string;
+    srcImgMobile: string;
     children: React.ReactNode
 }
-export const CoverImage = ({ width, height, srcImg, children }: CoverImageProps) =>  {
+export const CoverImage = ({ width, height, srcImg, srcImgMobile, children }: CoverImageProps) =>  {
     const parallaxRef = useRef<HTMLDivElement>(null);
     return (
         <section className="border-b-4 border-solid relative overlay overflow-hidden gradient-border inverted before:bg-[radial-gradient(rgba(255,255,255,0.8)_0%,_rgba(255,255,255,0.55)_100%),_linear-gradient(-30deg,_rgba(0,247,255,0.08)_0%,_#0000_15%,_#0000_80%,_rgba(0,247,255,0.08)_100%)]" ref={parallaxRef}>
-            <Image src={srcImg} width={width} height={height} sizes="(max-width: 768px) 100vw, (max-width: 1024px) 115vw, 130vw" alt="" priority={true} className="max-w-none w-[130vw] h-[120vh] absolute -z-10 object-cover blur-[0.047rem] saturate-[1.4]" />
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={srcImg} srcSet={`${srcImgMobile} 768w, ${srcImg} 1024w`} width={width} height={height} sizes="(max-width: 768px) 100vw, (max-width: 1024px) 115vw, 130vw" alt="" fetch-priority={true} className="max-w-none w-[130vw] h-[120vh] absolute -z-10 object-cover saturate-[1.4] blur-[0.047rem] sm:blur-0" />
             <div className="max-w-screen-xl min-h-[74vh] py-[0.1px]">
                 <div className="m-[18vh_1.25rem_6.625rem_0] py-5 text-left md:w-3/5 md:translate-y-[1.25rem]">
                     {children}
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <SchemaOrg headline="Riley Hoffman - Web Developer" description="I am a web developer with a passion for building accessible and responsive web applications. I quickly learn new concepts and love adding to my growing skill set. I am a proactive problem solver who enjoys writing future-proof, understandable code that fosters collaboration with other developers." image="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/thumbnail.jpg" datePublished="2024-07-04T09:25:01.340Z" />
-      <CoverImage width={1920} height={1080} srcImg="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/clouds2.webp">
+      <CoverImage width={1920} height={1080} srcImg="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/clouds3.webp" srcImgMobile="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/clouds2.webp">
           <FirstFoldContent />
       </CoverImage>
       <MyJourney /> 
