1b24dfabb8923d1d08a71d4e7fe9d42857d86b83
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-25 09:29:35 -0400

    CoverImage: fix fetchPriority attribute

--- app/components/home/CoverImage.tsx
+++ app/components/home/CoverImage.tsx
@@ -17,7 +17,7 @@ export const CoverImage = ({ width, height, srcImg, srcImgMobile, children }: Co
     return (
         <section className="border-b-4 border-solid relative overlay overflow-hidden gradient-border inverted before:bg-[radial-gradient(rgba(255,255,255,0.8)_0%,_rgba(255,255,255,0.55)_100%),_linear-gradient(-30deg,_rgba(0,247,255,0.08)_0%,_#0000_15%,_#0000_80%,_rgba(0,247,255,0.08)_100%)]" ref={parallaxRef}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={srcImg} srcSet={`${srcImgMobile} 768w, ${srcImg} 1024w`} width={width} height={height} sizes="(max-width: 768px) 100vw, (max-width: 1024px) 115vw, 130vw" alt="" fetch-priority={true} className="max-w-none w-[130vw] h-[120vh] absolute -z-10 object-cover saturate-[1.4] blur-[0.047rem] sm:blur-0" />
+            <img src={srcImg} srcSet={`${srcImgMobile} 768w, ${srcImg} 1024w`} width={width} height={height} sizes="(max-width: 768px) 100vw, (max-width: 1024px) 115vw, 130vw" alt="" fetchPriority="high" className="max-w-none w-[130vw] h-[120vh] absolute -z-10 object-cover saturate-[1.4] blur-[0.047rem] sm:blur-0" />
             <div className="max-w-screen-xl min-h-[74vh] py-[0.1px]">
                 <div className="m-[18vh_1.25rem_6.625rem_0] py-5 text-left md:w-3/5 md:translate-y-[1.25rem]">
                     {children}
