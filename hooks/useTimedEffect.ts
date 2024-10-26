16ee49a5af9d65c711cdd9a5325a83cae36cb353
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-10-26 10:38:13 -0400

    Hooks: added better cleanup and unmounted for 3 hooks

--- hooks/useTimedEffect.ts
+++ hooks/useTimedEffect.ts
@@ -1,18 +1,27 @@
 import { useRef } from 'react';
 
 export const useTimedEffect = (duration: number = 1000) => {
-    const isCountingRef = useRef(false);
-    
-    const triggerEffect = (applyEffect: (state: boolean) => void) => {
-        if (!isCountingRef.current) {
-            isCountingRef.current = true;
-            applyEffect(true);
+  const isCountingRef = useRef(false);
+  const timeoutId = useRef<NodeJS.Timeout | null>(null);
 
-            setTimeout(() => {
-                isCountingRef.current = false;
-                applyEffect(false);
-            }, duration);
-        }
-    };
-    return { triggerEffect };
+  const triggerEffect = (applyEffect: (state: boolean) => void) => {
+    if (!isCountingRef.current) {
+      isCountingRef.current = true;
+      applyEffect(true);
+
+      timeoutId.current = setTimeout(() => {
+        isCountingRef.current = false;
+        applyEffect(false);
+      }, duration);
+    }
+  };
+
+  return { 
+    triggerEffect,
+    cleanup: () => {
+      if (timeoutId.current) {
+        clearTimeout(timeoutId.current);
+      }
+    }
+  };
 };
