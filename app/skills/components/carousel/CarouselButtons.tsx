54350cd666d0624467186ec446a68bd57a013117
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-02 09:35:57 -0400

    CarouselButtons: remove inset shadow on active

--- app/skills/components/carousel/CarouselButtons.tsx
+++ app/skills/components/carousel/CarouselButtons.tsx
@@ -8,13 +8,13 @@ export const CarouselButtons = () => {
     <>
       <button
         aria-label="Previous slide"
-        className={`prev-btn left-4 shadow-[#12121c_2px_3px_0_0,_inset_#12121c_0_0_0_0] hover:shadow-[#12121c_1px_1px_0_0,_inset_#12121c_0_0_0_0] focus-visible:shadow-[#12121c_1px_1px_0_0,_inset_#12121c_0_0_0_0] active:shadow-[#12121c_0_0_0_0,_inset_#12121c_1px_1px_3px_-1px] ${commonClasses}`}
+        className={`prev-btn left-4 shadow-[#12121c_2px_3px_0_0,_inset_#12121c_0_0_0_0] hover:shadow-[#12121c_1px_1px_0_0,_inset_#12121c_0_0_0_0] focus-visible:shadow-[#12121c_1px_1px_0_0,_inset_#12121c_0_0_0_0] active:shadow-none ${commonClasses}`}
       >
         <FontAwesomeIcon icon={faArrowLeft} />
       </button>
       <button
         aria-label="Next slide"
-        className={`next-btn right-4 shadow-[#12121c_-2px_3px_0_0] hover:shadow-[#12121c_-1px_1px_0_0,_inset_#12121c_0_0_0_0] focus-visible:shadow-[#12121c_-1px_1px_0_0,_inset_#12121c_0_0_0_0] active:shadow-[#12121c_0_0_0_0,inset_#12121c_-1px_1px_3px_-1px] ${commonClasses}`}
+        className={`next-btn right-4 shadow-[#12121c_-2px_3px_0_0] hover:shadow-[#12121c_-1px_1px_0_0,_inset_#12121c_0_0_0_0] focus-visible:shadow-[#12121c_-1px_1px_0_0,_inset_#12121c_0_0_0_0] active:shadow-none ${commonClasses}`}
       >
         <FontAwesomeIcon icon={faArrowRight} />
       </button>
