9ef68289decfa1a5e532c8308b532b13c04f0da8
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-04 10:02:36 -0500

    next.config.js: removed  remotePatterns and headers for images and unoptimized is true. netlify.toml: removed _next/image header cached policy

--- next.config.js
+++ next.config.js
@@ -6,29 +6,7 @@ const nextConfig = {
     },
     images: {
         unoptimized: true,
-        formats: ['image/avif', 'image/webp'],
-
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-                pathname: '/**',
-            },
-        ],
     },
-    async headers() {
-        return [
-            {
-                source: '/_next/image',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                ],
-            },
-        ];
-    }
 }
 
 module.exports = nextConfig
