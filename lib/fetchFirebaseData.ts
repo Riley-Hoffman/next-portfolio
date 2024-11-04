37cae8eccba3e8096c6a03b01349a5bf4a5f3210
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-04 11:25:35 -0500

    firebaseConfig: combined into one file with fetchFirebaseData.ts.

--- lib/fetchFirebaseData.ts
+++ lib/fetchFirebaseData.ts
@@ -1,5 +1,15 @@
-import { db } from "./firebaseConfig";
-import { ref, get } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, get } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
 export async function fetchFirebaseData<T>(path: string): Promise<T | null> {
   try {
