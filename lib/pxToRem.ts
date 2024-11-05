6ef1824141ab8a1fa6dc88209526083ed1a790bb
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-05 12:22:04 -0500

    pxToRem.ts: formatting

--- lib/pxToRem.ts
+++ lib/pxToRem.ts
@@ -1,9 +1,8 @@
 export const getRootFontSize = () => {
-    return parseFloat(getComputedStyle(document.documentElement).fontSize);
-  };
-  
-  export const pxToRem = (px: number) => {
-    const rootFontSize = getRootFontSize();
-    return px / rootFontSize;
-  };
-  
+  return parseFloat(getComputedStyle(document.documentElement).fontSize);
+};
+
+export const pxToRem = (px: number) => {
+  const rootFontSize = getRootFontSize();
+  return px / rootFontSize;
+};
