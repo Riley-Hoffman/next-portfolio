b3006e204fb0171dbd7d3e2a86da5298f509b513
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-07 12:07:33 -0500

    accessibility page: fixed typo schema url typo

--- app/accessibility/page.tsx
+++ app/accessibility/page.tsx
@@ -1,32 +1,35 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import { SchemaGenerator } from "../components/SchemaGenerator";
-import {
-  getBaseUrl,
-  getImageUrl,
-} from "../../lib/constants";
+import { getBaseUrl, getImageUrl } from "../../lib/constants";
 
-const description = "As a dedicated web developer, I am committed to creating an accessible and inclusive website experience for all users.";
+const description =
+  "As a dedicated web developer, I am committed to creating an accessible and inclusive website experience for all users.";
 
 export const metadata: Metadata = {
   title: "Accessibility",
-  description:
-    description,
-  metadataBase: new URL(`${getBaseUrl('/accessibility')}`),
+  description: description,
+  metadataBase: new URL(`${getBaseUrl("/accessibility")}`),
   alternates: {
     canonical: "/",
   },
   openGraph: {
     images: getImageUrl(),
     title: "Accessibility",
-    url: `${getBaseUrl('/accessibility')}`,
+    url: `${getBaseUrl("/accessibility")}`,
   },
 };
 
 export default function Accessibility() {
   return (
     <>
-      <SchemaGenerator title="Accessibility" description={description} urlPath="/conaccessibilitytact" publishDate="2024-08-07T09:25:01.340Z" schemaType="WebPage" />
+      <SchemaGenerator
+        title="Accessibility"
+        description={description}
+        urlPath="/accessibility"
+        publishDate="2024-08-07T09:25:01.340Z"
+        schemaType="WebPage"
+      />
       <h1 className="gradient-border inverted mb-7 mt-0 border-b-2 bg-[#eee2f3] px-5 py-10 text-center text-3xl leading-normal contrast-more:bg-white md:text-5xl md:leading-normal">
         Accessibility
       </h1>
