565a5085e4bbadafc58c863cfa40a4981c523f15
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-19 09:57:13 -0500

    Added tool tip to SiteTechStack links. Removed scroll animation.

--- app/components/home/SiteTechStack.tsx
+++ app/components/home/SiteTechStack.tsx
@@ -1,5 +1,3 @@
-import { useMemo, useEffect } from "react"
-import { useTriggerOnScroll } from "../../../hooks/useTriggerOnScroll"
 import { NewTabSrText } from "../../components/NewTabSrText"
 
 interface Tech {
@@ -32,31 +30,6 @@ export const SiteTechStack = () => {
       url: "https://firebase.google.com",
     },
   ]
-  const elementsRef = useTriggerOnScroll()
-  const animation = useMemo(
-    () => [
-      "motion-safe:md:h-[0rem]",
-      "[&[data-active='true']]:h-[10rem]",
-      "transition-all",
-      "first:duration-150",
-      "[&:nth-child(2)]:duration-300",
-      "[&:nth-child(3)]:duration-500",
-      "[&:nth-child(4)]:duration-700",
-      "last:duration-1000",
-      "ease",
-    ],
-    []
-  )
-  useEffect(() => {
-    if (elementsRef.current) {
-      elementsRef.current.forEach((element: HTMLElement) => {
-        animation.forEach((className) => {
-          element.classList.add(className)
-        })
-      })
-    }
-  }, [animation, elementsRef])
-
   return (
     <>
       <h2 className="reg-caps mb-8 font-inconsolata text-2xl">
@@ -72,14 +45,9 @@ export const SiteTechStack = () => {
             className={`relative sm:col-auto lg:col-auto ${
               index === technologies.length - 2 && "md:col-start-1"
             } ${index === technologies.length - 1 && "md:col-start-3"}`}
-            ref={(el) => {
-              if (el) {
-                elementsRef.current.push(el)
-              }
-            }}
           >
             <a
-              className="mx-auto block min-h-[9.5rem] w-fit min-w-[13.1rem] rounded-md bg-pink-100 p-5 no-underline shadow-[#12121c_0.25rem_0.25rem_0_0_inset_#12121c_0_0_150px_0] transition-shadow duration-200 hover:bg-zinc hover:text-pink-100 focus-visible:bg-zinc focus-visible:text-pink-100 active:min-h-fit motion-safe:md:hover:bg-pink-100 motion-safe:md:hover:shadow-[inset_#12121c_100vw_100vh_150px_0] motion-safe:md:hover:duration-1000 motion-safe:md:focus-visible:bg-pink-100 motion-safe:md:focus-visible:shadow-[inset_#12121c_100vw_100vh_150px_0] motion-safe:md:focus-visible:duration-1000"
+              className="tool-tip mx-auto block min-h-[9.5rem] w-fit min-w-[13.1rem] rounded-md bg-pink-100 p-5 no-underline shadow-[#12121c_0.25rem_0.25rem_0_0_inset_#12121c_0_0_150px_0] transition-shadow duration-200 before:border-t-pink-100 after:left-0 after:bg-pink-100 after:leading-5 after:text-zinc after:content-['Opens_new_window'] hover:bg-zinc hover:text-pink-100 focus-visible:bg-zinc focus-visible:text-pink-100 active:min-h-fit motion-safe:md:hover:bg-pink-100 motion-safe:md:hover:shadow-[inset_#12121c_100vw_100vh_150px_0] motion-safe:md:hover:duration-1000 motion-safe:md:hover:before:duration-700 motion-safe:md:hover:after:duration-700 motion-safe:md:focus-visible:bg-pink-100 motion-safe:md:focus-visible:shadow-[inset_#12121c_100vw_100vh_150px_0] motion-safe:md:focus-visible:duration-1000"
               href={tech.url}
               target="_blank"
               rel="noopener noreferrer"
