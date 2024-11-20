60345d8a2230e8db36937dc06bff00b18dae6929
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-11-20 12:58:35 -0500

    Accordion: debounced clicks to ensure smooth animation

--- app/faq/components/Accordion.tsx
+++ app/faq/components/Accordion.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useAccordion } from "../../../hooks/useAccordion"
+import { useRef } from "react"
 import "./styles/accordion.css"
 
 type AccordionItem = {
@@ -15,6 +16,19 @@ type AccordionProps = {
 export const Accordion = ({ items, label }: AccordionProps) => {
   const { openIndex, handleAccordionClick, buttonRefs, contentRefs } =
     useAccordion(items.length)
+
+  const debounceRef = useRef<NodeJS.Timeout | null>(null)
+
+  const handleDebouncedClick = (index: number) => {
+    if (debounceRef.current) return
+
+    handleAccordionClick(index)
+
+    debounceRef.current = setTimeout(() => {
+      debounceRef.current = null
+    }, 500)
+  }
+
   return (
     <ul
       className="mb-12 max-w-[90%] rounded-lg leading-loose shadow-[#12121c_0.25rem_0.25rem_0_0]"
@@ -27,7 +41,7 @@ export const Accordion = ({ items, label }: AccordionProps) => {
               buttonRefs.current[index] = el
             }}
             className="accordion group peer w-full border-b border-solid border-[#e1d7ee] bg-pink-100 px-5 py-6 text-left text-lg font-medium leading-9 text-[#2d2b34] transition-all duration-500 ease-in-out group-first:rounded-t-lg group-last:rounded-b-lg group-last:border-b-0 aria-expanded:border-b-0"
-            onClick={() => handleAccordionClick(index)}
+            onClick={() => handleDebouncedClick(index)}
             aria-expanded={openIndex === index ? true : false}
           >
             <div className="plus-minus mb-[0.063rem] mr-5 box-content rounded-[50%] bg-zinc p-3 text-xs text-pink-200 transition-all duration-200 ease-in-out group-hover:bg-purple-100 group-hover:text-zinc group-focus-visible:bg-purple-100 group-focus-visible:text-zinc group-aria-expanded:bg-purple-100 group-aria-expanded:text-zinc"></div>
