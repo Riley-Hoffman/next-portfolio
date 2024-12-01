534b0ae1fe94b0e8f3e644c0f57c43693fa8aaa6
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-01 09:37:32 -0500

    Moved favicon/icon pngs to public/icons. layout.tsx: added links to sized favicons.

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" sizes="512x512" href="/android-chrome-256x256.png" />
-        <link rel="icon" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+        <link rel="icon" sizes="512x512" href="/icons/android-chrome-256x256.png" />
+        <link rel="icon" sizes="512x512" href="/icons/android-chrome-512x512.png" />
         <noscript>
           <style>{`.nojslayout.hidden {display: block}`}</style>
         </noscript>
