b463731bb8fdfd50fc7dc2089d884af1e9789f5c
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-02 11:54:25 -0500

    MyJourney: added image schema.

--- app/components/home/MyJourney.tsx
+++ app/components/home/MyJourney.tsx
@@ -21,20 +21,25 @@ export const MyJourney = () => {
   return (
     <section
       ref={sectionRef}
-      className="scroll-color motion-reduce:scroll-color:hue-rotate-0 bg-diamonds pb-10 pt-16 contrast-more:bg-none dark:bg-diamondsdark"
-      style={{ filter: `hue-rotate(0deg)` }}
+      className="scroll-color bg-diamonds pb-10 pt-16 motion-reduce:hue-rotate-0 contrast-more:bg-none dark:bg-diamondsdark"
     >
       <div className="max-w-screen-xl">
         <div className="items-center md:flex">
           <div className="min-h-96 drop-shadow-[0_0_0.063rem_#877ADB] md:w-1/3">
-            <Image
-              className="size-96 max-w-full rounded-3xl object-cover"
-              src="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/headshot.webp"
-              width={384}
-              height={452}
-              alt="Riley with a flower behind his ear. Shot in black and white."
-              priority={false}
-            />
+            <figure itemScope itemType="https://schema.org/ImageObject">
+              <Image
+                className="size-96 max-w-full rounded-3xl object-cover"
+                src="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/headshot.webp"
+                width={384}
+                height={452}
+                alt="Riley with a flower behind his ear. Shot in black and white."
+                priority={false}
+                itemProp="contentUrl"
+              />
+              <figcaption className="sr-only" itemProp="creditText">
+                Photo by Kristen Mommertz
+              </figcaption>
+            </figure>
           </div>
           <div className="m-6 border-2 border-[#d6d2ee] bg-[whitesmoke] py-6 contrast-more:bg-white dark:bg-siteblack md:w-2/3 lg:px-24">
             <h2>My Journey</h2>
