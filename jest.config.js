71abcc12c396a4e2f76291baf6644e62f85a6b2a
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-06 11:51:55 -0500

    jest.config.js: change testingenvironment to jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
