936bf342b2d50405a6848bb670037bb76456148c
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-11 11:37:10 -0500

    jest.config.js update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
-/** @type {import('jest').Config} */
-module.exports = {
+const nextJest = require("next/jest.js")
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+const config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
 }
+
+module.exports = async () => ({
+  ...(await createJestConfig(config)()),
+  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
+})
