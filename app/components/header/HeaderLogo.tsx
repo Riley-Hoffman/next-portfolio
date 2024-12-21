a26ed006cfccb8743712425ea98a8a19619ceaa6
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-21 18:24:55 -0500

    HeaderLogo: added useRouter to handle home navigation.

--- app/components/header/HeaderLogo.tsx
+++ app/components/header/HeaderLogo.tsx
@@ -1,14 +1,13 @@
+import { useRouter } from "next/navigation"
+
 export const HeaderLogo = () => {
-  const handleClickHome = () => {
-    if (typeof window !== "undefined") {
-      window.location.href = "/"
-    }
-  }
+  const router = useRouter()
+
   return (
     <div className="logo">
       <button
         className="order-1 m-0 pl-4 pr-0 text-center font-urbanist text-lg font-medium uppercase tracking-wider md:font-normal lg:text-2xl"
-        onClick={handleClickHome}
+        onClick={() => router.push("/")}
       >
         <span aria-hidden={true}>Riley Hoffman</span>
         <span className="sr-only">Back To Home Page</span>
