fe05352693a1f19777f2c8fcf927e45016791349
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-22 12:18:32 -0500

    useRoutes: using meta data in Route array for filtering instead of ternary in filter()

--- hooks/useRoutes.ts
+++ hooks/useRoutes.ts
@@ -3,16 +3,17 @@ import { useMemo } from "react"
 interface Route {
   to: string
   label: string
+  includeInFilter: boolean
 }
 
 export const RouteList: Route[] = [
-  { to: "/", label: "Home" },
-  { to: "/projects/", label: "Projects" },
-  { to: "/projects/particle-cleanup/", label: "Particle Cleanup Game" },
-  { to: "/skills/", label: "Skills" },
-  { to: "/faq/", label: "FAQ" },
-  { to: "/contact/", label: "Contact" },
-  { to: "/accessibility/", label: "Accessibility" },
+  { to: "/", label: "Home", includeInFilter: true },
+  { to: "/projects/", label: "Projects", includeInFilter: true },
+  { to: "/projects/particle-cleanup/", label: "Particle Cleanup Game", includeInFilter: false },
+  { to: "/skills/", label: "Skills", includeInFilter: true },
+  { to: "/faq/", label: "FAQ", includeInFilter: true },
+  { to: "/contact/", label: "Contact", includeInFilter: true },
+  { to: "/accessibility/", label: "Accessibility", includeInFilter: false },
 ]
 
 type FilterMode = "all" | "filtered"
@@ -20,11 +21,7 @@ type FilterMode = "all" | "filtered"
 export const useRoutes = (filterMode: FilterMode): Route[] => {
   return useMemo(() => {
     if (filterMode === "filtered") {
-      return RouteList.filter(
-        (route) =>
-          route.label !== "Particle Cleanup Game" &&
-          route.label !== "Accessibility"
-      )
+      return RouteList.filter((route) => route.includeInFilter)
     }
     return RouteList
   }, [filterMode])
