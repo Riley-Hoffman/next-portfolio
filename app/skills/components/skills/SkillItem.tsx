2940035370351b00fe6b83ab4685a339e963bb0d
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-30 09:35:32 -0500

    Skills: updated icons.

--- app/skills/components/skills/SkillItem.tsx
+++ app/skills/components/skills/SkillItem.tsx
@@ -1,20 +1,10 @@
-import Image from "next/image"
 import { Icon } from "@iconify/react"
 import { Skill } from "./SkillsData"
 
-export const SkillItem = ({ skill, icon, image }: Skill) => (
+export const SkillItem = ({ skill, icon, classes }: Skill) => (
   <li className="text-center">
     <div className="skill-icon-box flex min-h-[2.766rem] items-center justify-center">
-      {icon && <Icon className="text-5xl" icon={icon} />}
-      {image && (
-        <Image
-          src={image}
-          className="w-20 selection:bg-transparent dark:invert"
-          width={300}
-          height={71}
-          alt=""
-        />
-      )}
+      {icon && <Icon className={`text-5xl outline-black${classes}`} icon={icon} />}
     </div>
     <p className="mt-1 text-xl" translate="no">
       {skill}
--- app/skills/components/skills/SkillsData.ts
+++ app/skills/components/skills/SkillsData.ts
@@ -1,38 +1,50 @@
-interface SkillBase {
+export interface Skill {
   skill: string
+  icon: string
+  classes?: string
 }
 
-export type Skill =
-  | (SkillBase & { icon: string; image?: never })
-  | (SkillBase & { image: string; icon?: never })
+const sharedSkillClasses = " p-[0.313rem] rounded-2xl"
 
 export const skillsData: Skill[] = [
-  { skill: "HTML5", icon: "devicon-plain:html5" },
-  { skill: "CSS3", icon: "devicon-plain:css3" },
-  { skill: "Javascript + ES6", icon: "devicon-plain:javascript" },
-  { skill: "Typescript", icon: "cib:typescript" },
-  { skill: "SASS", icon: "cib:sass-alt" },
-  { skill: "Tailwind CSS", icon: "bxl:tailwind-css" },
-  { skill: "React", icon: "cib:react" },
-  { skill: "Next.js", icon: "devicon-plain:nextjs" },
-  { skill: "Vue.js", icon: "devicon-plain:vuejs" },
-  { skill: "Rest API", icon: "fa6-solid:arrows-left-right" },
+  { skill: "HTML5", icon: "skill-icons:html" },
+  { skill: "CSS3", icon: "skill-icons:css" },
+  { skill: "Javascript + ES6", icon: "skill-icons:javascript" },
+  { skill: "Typescript", icon: "skill-icons:typescript" },
+  { skill: "SASS", icon: "skill-icons:sass" },
+  { skill: "Tailwind CSS", icon: "skill-icons:tailwindcss-dark" },
+  { skill: "React", icon: "skill-icons:react-dark" },
+  { skill: "Next.js", icon: "skill-icons:nextjs-dark" },
+  { skill: "Vue.js", icon: "skill-icons:vuejs-dark" },
+  {
+    skill: "Rest API",
+    icon: "catppuccin:api-blueprint",
+    classes: `${sharedSkillClasses} bg-[#edff74]`,
+  },
   {
     skill: "WCAG",
-    image:
-      "https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/wcag.png",
+    icon: "file-icons:w3c",
+    classes: `${sharedSkillClasses} bg-[#005a9c] text-white`,
   },
-  { skill: "Git", icon: "devicon-plain:git" },
-  { skill: "jQuery", icon: "devicon-plain:jquery" },
-  { skill: "Wordpress", icon: "devicon-plain:wordpress" },
-  { skill: "PHP", icon: "devicon-plain:php" },
-  { skill: "Firebase", icon: "bxl:firebase" },
-  { skill: "Netlify", icon: "devicon-plain:netlify" },
-  { skill: "Node.js", icon: "devicon-plain:nodejs" },
-  { skill: "SEO", icon: "fa6-solid:magnifying-glass-chart" },
-  { skill: "Express", icon: "devicon:express" },
-  { skill: "MongoDB", icon: "devicon-plain:mongodb" },
-  { skill: "GitHub Actions", icon: "devicon-plain:githubactions" },
-  { skill: "Elasticsearch", icon: "devicon-plain:elasticsearch" },
-  { skill: "Jest", icon: "devicon-plain:jest" },
+  { skill: "Git", icon: "skill-icons:git" },
+  { skill: "jQuery", icon: "skill-icons:jquery" },
+  { skill: "Wordpress", icon: "skill-icons:wordpress" },
+  { skill: "PHP", icon: "skill-icons:php-dark" },
+  {
+    skill: "Firebase",
+    icon: "devicon:firebase",
+    classes: `${sharedSkillClasses} bg-white`,
+  },
+  { skill: "Netlify", icon: "skill-icons:netlify-dark" },
+  { skill: "Node.js", icon: "skill-icons:nodejs-dark" },
+  {
+    skill: "SEO",
+    icon: "fa6-solid:magnifying-glass-chart",
+    classes: `${sharedSkillClasses} bg-[#FBBC05] text-[#4285F4]`,
+  },
+  { skill: "Express", icon: "skill-icons:expressjs-dark" },
+  { skill: "MongoDB", icon: "skill-icons:mongodb" },
+  { skill: "GitHub Actions", icon: "skill-icons:githubactions-dark" },
+  { skill: "Elasticsearch", icon: "skill-icons:elasticsearch-dark" },
+  { skill: "Jest", icon: "skill-icons:jest" },
 ]
