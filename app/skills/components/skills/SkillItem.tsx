c49d65083054f2b2a197515c4208a78be25abe65
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-30 10:25:23 -0500

    SiteTechStack: added before:content-none to hide arrow shape after hover.

--- app/skills/components/skills/SkillItem.tsx
+++ app/skills/components/skills/SkillItem.tsx
@@ -4,7 +4,9 @@ import { Skill } from "./SkillsData"
 export const SkillItem = ({ skill, icon, classes }: Skill) => (
   <li className="text-center">
     <div className="skill-icon-box flex min-h-[2.766rem] items-center justify-center">
-      {icon && <Icon className={`text-5xl outline-black${classes}`} icon={icon} />}
+      {icon && (
+        <Icon className={`text-5xl outline-black${classes}`} icon={icon} />
+      )}
     </div>
     <p className="mt-1 text-xl" translate="no">
       {skill}
