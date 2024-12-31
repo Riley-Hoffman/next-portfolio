42a6d04cd72d4ce0d66eac0b0de7f05275d9c043
Riley Hoffman <riley.n.hoffman@gmail.com>
2024-12-31 11:55:10 -0500

    ThemeChanger: added check to default to user system preference so correct icon is always displayed on first page load.

--- app/components/header/ThemeChanger.tsx
+++ app/components/header/ThemeChanger.tsx
@@ -2,19 +2,26 @@ import { useTheme } from "next-themes"
 import { Icon } from "@iconify/react"
 
 const ThemeChanger = () => {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme, resolvedTheme } = useTheme()
+
+  const currentTheme = theme === "system" ? resolvedTheme : theme
+
+  if (!currentTheme) return null
+
+  const themeIcon =
+    currentTheme === "dark" ? "solar:sun-2-bold" : "solar:moon-bold"
+  const themeLabel =
+    currentTheme === "dark" ? "Switch to Light Theme" : "Switch to Dark Theme"
 
   return (
     <button
-      onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}
+      onClick={() => setTheme(currentTheme === "light" ? "dark" : "light")}
       className="relative mx-2 h-8 w-16 rounded-3xl bg-accentone-200 px-4 py-2 md:order-3"
-      aria-label={
-        theme === "dark" ? "Switch to Light Theme" : "Switch to Dark Theme"
-      }
+      aria-label={themeLabel}
     >
       <Icon
-        className="absolute left-1 top-1 h-6 w-6 rounded-full bg-accenttwo-200 p-1 text-sitebackground transition-transform dark:translate-x-8 dark:text-siteblack dark:content-['\\f185']"
-        icon={theme === "light" ? "solar:moon-bold" : "solar:sun-2-bold"}
+        className="absolute left-1 top-1 h-6 w-6 rounded-full bg-accenttwo-200 p-1 text-sitebackground transition-transform dark:translate-x-8 dark:text-siteblack"
+        icon={themeIcon}
       />
     </button>
   )
