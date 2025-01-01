fc6bfede8637a9ec4c7c42ad39c61b7df577ec14
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-01 08:21:00 -0500

    OvalDecor: moved to new components/decorative

--- app/projects/components/ProjectListItem.tsx
+++ app/projects/components/ProjectListItem.tsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react"
 import clsx from "clsx"
 import { useTriggerOnScroll } from "@/hooks/useTriggerOnScroll"
-import { OvalDecor } from "@/app/components/OvalDecor"
+import { OvalDecor } from "@/app/components/decorative/OvalDecor"
 
 interface ProjectBoxProps {
   inverted?: string
