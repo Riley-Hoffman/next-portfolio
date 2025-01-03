9e880075d7ae53e8b7585ebf892779c5a172fce9
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-03 10:21:49 -0500

    PlayAgain.tsx: gave button Verdana font

--- app/projects/particle-cleanup/components/PlayAgain.tsx
+++ app/projects/particle-cleanup/components/PlayAgain.tsx
@@ -4,7 +4,7 @@ type PlayAgainProps = {
 
 export const PlayAgain = ({ reloadAnimation }: PlayAgainProps) => (
   <button
-    className="mx-5 mb-5 mt-0 rounded-b-sm bg-[#2b2b64] p-3 text-white hover:bg-[#121226] focus-visible:bg-[#121226]"
+    className="mx-5 mb-5 mt-0 rounded-b-sm bg-[#2b2b64] p-3 font-['Verdana','Geneva','sans-serif'] text-white hover:bg-[#121226] focus-visible:bg-[#121226]"
     onClick={reloadAnimation}
   >
     Play Again
