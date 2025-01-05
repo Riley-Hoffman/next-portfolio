4a8f6b4070c40da0e302406bc34c95ab7b6a8661
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-05 11:11:38 -0500

    Carousel: Moved buttonsConfig to carousel/constants folder

--- app/skills/components/trainings/carousel/CarouselButtons.tsx
+++ app/skills/components/trainings/carousel/CarouselButtons.tsx
@@ -1,20 +1,6 @@
 import { useEffect, useRef } from "react"
 import { Icon } from "@iconify/react"
-
-const buttonsConfig = [
-  {
-    label: "Previous slide",
-    className:
-      "prev-btn left-4 hidden shadow-[#12121c_0.125rem_0.188rem_0_0] dark:shadow-[#edede3_0.125rem_0.188rem_0_0]",
-    icon: "raphael:arrowleft",
-  },
-  {
-    label: "Next slide",
-    className:
-      "next-btn right-4 hidden shadow-[#12121c_-0.125rem_0.188rem_0_0] dark:shadow-[#edede3_-0.125rem_0.188rem_0_0]",
-    icon: "raphael:arrowright",
-  },
-]
+import { buttonsConfig } from "./constants/buttonsConfig"
 
 export const CarouselButtons = () => {
   const buttonsRef = useRef<(HTMLButtonElement | null)[]>([])
