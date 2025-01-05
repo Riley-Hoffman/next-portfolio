0d3f52e564651f769d19df6d0c97191615b268ff
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-05 11:07:46 -0500

    CarouselButtons: rendered buttons using map + config array

--- app/skills/components/trainings/carousel/CarouselButtons.tsx
+++ app/skills/components/trainings/carousel/CarouselButtons.tsx
@@ -1,6 +1,21 @@
 import { useEffect, useRef } from "react"
 import { Icon } from "@iconify/react"
 
+const buttonsConfig = [
+  {
+    label: "Previous slide",
+    className:
+      "prev-btn left-4 hidden shadow-[#12121c_0.125rem_0.188rem_0_0] dark:shadow-[#edede3_0.125rem_0.188rem_0_0]",
+    icon: "raphael:arrowleft",
+  },
+  {
+    label: "Next slide",
+    className:
+      "next-btn right-4 hidden shadow-[#12121c_-0.125rem_0.188rem_0_0] dark:shadow-[#edede3_-0.125rem_0.188rem_0_0]",
+    icon: "raphael:arrowright",
+  },
+]
+
 export const CarouselButtons = () => {
   const buttonsRef = useRef<(HTMLButtonElement | null)[]>([])
 
@@ -14,24 +29,18 @@ export const CarouselButtons = () => {
 
   return (
     <>
-      <button
-        ref={(el) => {
-          buttonsRef.current[0] = el
-        }}
-        aria-label="Previous slide"
-        className={`prev-btn left-4 hidden shadow-[#12121c_0.125rem_0.188rem_0_0] dark:shadow-[#edede3_0.125rem_0.188rem_0_0]`}
-      >
-        <Icon icon="raphael:arrowleft" />
-      </button>
-      <button
-        ref={(el) => {
-          buttonsRef.current[1] = el
-        }}
-        aria-label="Next slide"
-        className={`next-btn right-4 hidden shadow-[#12121c_-0.125rem_0.188rem_0_0] dark:shadow-[#edede3_-0.125rem_0.188rem_0_0]`}
-      >
-        <Icon icon="raphael:arrowright" />
-      </button>
+      {buttonsConfig.map((config, index) => (
+        <button
+          key={index}
+          ref={(el) => {
+            buttonsRef.current[index] = el
+          }}
+          aria-label={config.label}
+          className={config.className}
+        >
+          <Icon icon={config.icon} />
+        </button>
+      ))}
     </>
   )
 }
