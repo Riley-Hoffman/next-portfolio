7fa387b88ef93c2d5eeb8a0b84f5b172b5a1e649
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-06 11:01:07 -0500

    DynamicThemeProvider: removed unecessary React import

--- app/components/theme/DynamicThemeProvider.tsx
+++ app/components/theme/DynamicThemeProvider.tsx
@@ -1,6 +1,6 @@
 "use client"
 import dynamic from "next/dynamic"
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import type { ThemeProviderProps } from "next-themes"
 
 const NextThemesProvider = dynamic(
