a9fd783cab2e72f61b5f633e454d9204ca4bcb1c
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-07 08:38:35 -0500

    Accordion: added const isOpen = accOpen(index) to pass children

--- app/faq/components/accordion/Accordion.tsx
+++ app/faq/components/accordion/Accordion.tsx
@@ -19,29 +19,32 @@ export const Accordion = ({ items, label }: AccordionProps) => {
       className="mb-12 max-w-[90%] rounded-lg leading-loose shadow-[#12121c_0.25rem_0.25rem_0_0] dark:shadow-[#edede3_0.25rem_0.25rem_0_0]"
       aria-label={label}
     >
-      {items.map((item, index) => (
-        <li className="group" key={index}>
-          <AccordionQuestion
-            data={{
-              question: item.question,
-              isOpen: accOpen(index),
-              onClick: () => handleAccordionClick(index),
-              buttonRef: (el) => {
-                if (el) buttonRefs.current[index] = el
-              },
-            }}
-          />
-          <AccordionAnswer
-            data={{
-              answer: item.answer,
-              isOpen: accOpen(index),
-              contentRef: (el) => {
-                if (el) contentRefs.current[index] = el
-              },
-            }}
-          />
-        </li>
-      ))}
+      {items.map((item, index) => {
+        const isOpen = accOpen(index)
+        return (
+          <li className="group" key={index}>
+            <AccordionQuestion
+              data={{
+                question: item.question,
+                isOpen,
+                onClick: () => handleAccordionClick(index),
+                buttonRef: (el) => {
+                  if (el) buttonRefs.current[index] = el
+                },
+              }}
+            />
+            <AccordionAnswer
+              data={{
+                answer: item.answer,
+                isOpen,
+                contentRef: (el) => {
+                  if (el) contentRefs.current[index] = el
+                },
+              }}
+            />
+          </li>
+        )
+      })}
     </ul>
   )
 }
