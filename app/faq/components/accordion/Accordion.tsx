cd882be1a03dcf50b8b3a3cbfc936f97b816bbec
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-11 10:06:37 -0500

    Accordion: updated dark mode shadow color

--- app/faq/components/accordion/Accordion.tsx
+++ app/faq/components/accordion/Accordion.tsx
@@ -16,7 +16,7 @@ export const Accordion = ({ items, label }: AccordionProps) => {
 
   return (
     <ul
-      className="mb-[calc(13vh+3rem)] mt-14 max-w-prose rounded-lg leading-loose shadow-[0.25rem_0.25rem_0_0] shadow-heading dark:shadow-[#edede3]"
+      className="mb-[calc(13vh+3rem)] mt-14 max-w-prose rounded-lg leading-loose shadow-[0.25rem_0.25rem_0_0] shadow-heading dark:shadow-gradientthree"
       aria-label={label}
     >
       {items.map((item, index) => {
