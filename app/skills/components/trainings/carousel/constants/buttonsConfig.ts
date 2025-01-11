d5c473822d60e2ede7aca0e76e9d22af4fd83870
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-11 09:33:35 -0500

    Carousel: buttonsConfig.js, updated dark mode shadow color

--- app/skills/components/trainings/carousel/constants/buttonsConfig.ts
+++ app/skills/components/trainings/carousel/constants/buttonsConfig.ts
@@ -2,13 +2,13 @@ export const buttonsConfig = [
   {
     label: "Previous slide",
     className:
-      "prev-btn left-4 hidden shadow-[#12121c_0.125rem_0.188rem_0_0] dark:shadow-[#edede3_0.125rem_0.188rem_0_0]",
+      "prev-btn left-4 hidden shadow-[#12121c_0.125rem_0.188rem_0_0] dark:shadow-[#71b1b0_0.125rem_0.188rem_0_0]",
     icon: "raphael:arrowleft",
   },
   {
     label: "Next slide",
     className:
-      "next-btn right-4 hidden shadow-[#12121c_-0.125rem_0.188rem_0_0] dark:shadow-[#edede3_-0.125rem_0.188rem_0_0]",
+      "next-btn right-4 hidden shadow-[#12121c_-0.125rem_0.188rem_0_0] dark:shadow-[#71b1b0_-0.125rem_0.188rem_0_0]",
     icon: "raphael:arrowright",
   },
 ]
