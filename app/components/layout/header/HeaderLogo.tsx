89b47993da0fa4c6955a1fbf59b16b05490d2d2e
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-12 11:49:06 -0500

    HeaderLogo: removed <span> containing 'Riley Hoffman'

--- app/components/layout/header/HeaderLogo.tsx
+++ app/components/layout/header/HeaderLogo.tsx
@@ -9,7 +9,7 @@ export const HeaderLogo = () => {
         className="order-1 m-0 pl-4 pr-0 text-center font-urbanist font-medium uppercase tracking-wider text-heading md:font-normal lg:text-2xl"
         onClick={() => router.push("/")}
       >
-        <span aria-hidden={true}>Riley Hoffman</span>
+        Riley Hoffman
         <span className="sr-only">Back To Home Page</span>
       </button>
     </div>
