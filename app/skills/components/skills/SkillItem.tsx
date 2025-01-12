7420e4aed13da8e9ae7e32732b16d957b47523a8
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-12 10:41:30 -0500

    SkillItem: removed unnecessary div

--- app/skills/components/skills/SkillItem.tsx
+++ app/skills/components/skills/SkillItem.tsx
@@ -2,12 +2,10 @@ import { Icon } from "@iconify/react"
 import { Skill } from "./SkillsData"
 
 export const SkillItem = ({ skill, icon, classes }: Skill) => (
-  <li className="text-center">
-    <div className="flex min-h-11 items-center justify-center">
-      {icon && (
-        <Icon className={`text-5xl outline-black${classes}`} icon={icon} />
-      )}
-    </div>
+  <li className="text-center min-h-11">
+    {icon && (
+      <Icon className={`text-5xl outline-black${classes}`} icon={icon} />
+    )}
     <p className="mt-1 text-xl">{skill}</p>
   </li>
 )
