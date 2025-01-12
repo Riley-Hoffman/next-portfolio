ab09bdfb2545b9ed0988f9ea108d30bf286b22c6
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-12 12:27:47 -0500

    Footer: added pad-wrap class instead of container dv

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -3,34 +3,32 @@ import { Icon } from "@iconify/react"
 import { getPageTitle, githubUrl, version } from "@/constants/baseData"
 
 export const Footer = () => (
-  <footer className="gradient-border min-h-24 border-t border-solid bg-accentone-300 contrast-more:bg-white dark:bg-accentone-200">
+  <footer className="pad-wrap gradient-border min-h-24 border-t border-solid bg-accentone-300 py-7 contrast-more:bg-white dark:bg-accentone-200 md:flex md:items-center md:justify-between">
     <h2 className="sr-only">Footer</h2>
-    <div className="max-w-screen-xl py-7 md:flex md:items-center md:justify-between">
-      <p className="font-urbanist">
-        {getPageTitle()} <small>(v{version}) </small>
-        <a
-          className="group inline-block min-h-full no-underline md:tool-tip md:min-h-5 md:min-w-6"
-          href={githubUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Site repo on GitHub (Opens in a new tab)"
-        >
-          {" "}
-          <Icon
-            className="ml-1 text-3xl group-hover:text-accenttwo-200 group-focus-visible:text-accenttwo-200 md:text-base"
-            icon="cib:github"
-          />
-        </a>
-      </p>
-      <p className="mr-auto p-0 font-medium">
-        <Link className="px-3 py-1" href="/accessibility">
-          Accessibility
-        </Link>
-      </p>
-      <p>
-        <span className="mr-0.5">©</span>
-        {new Date().getFullYear()}
-      </p>
-    </div>
+    <p className="font-urbanist">
+      {getPageTitle()} <small>(v{version}) </small>
+      <a
+        className="group inline-block min-h-full no-underline md:tool-tip md:min-h-5 md:min-w-6"
+        href={githubUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Site repo on GitHub (Opens in a new tab)"
+      >
+        {" "}
+        <Icon
+          className="ml-1 text-3xl group-hover:text-accenttwo-200 group-focus-visible:text-accenttwo-200 md:text-base"
+          icon="cib:github"
+        />
+      </a>
+    </p>
+    <p className="mr-auto p-0 font-medium">
+      <Link className="px-3 py-1" href="/accessibility">
+        Accessibility
+      </Link>
+    </p>
+    <p>
+      <span className="mr-0.5">©</span>
+      {new Date().getFullYear()}
+    </p>
   </footer>
 )
