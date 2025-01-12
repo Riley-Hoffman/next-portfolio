ebed333541a31a3940d0167187f0f72d88158f6a
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-12 11:51:35 -0500

    HeaderLogo: removed div containing button element

--- app/components/layout/header/HeaderLogo.tsx
+++ app/components/layout/header/HeaderLogo.tsx
@@ -4,14 +4,13 @@ export const HeaderLogo = () => {
   const router = useRouter()
 
   return (
-    <div className="logo">
-      <button
-        className="order-1 m-0 pl-4 pr-0 text-center font-urbanist font-medium uppercase tracking-wider text-heading md:font-normal lg:text-2xl"
-        onClick={() => router.push("/")}
-      >
-        Riley Hoffman
-        <span className="sr-only">Back To Home Page</span>
-      </button>
-    </div>
+    <button
+      className="m-0 pl-4 pr-0 text-center font-urbanist font-medium uppercase tracking-wider text-heading md:font-normal lg:text-2xl"
+      onClick={() => router.push("/")}
+    >
+      Riley Hoffman
+      <span className="sr-only">Back To Home Page</span>
+    </button>
+
   )
 }
