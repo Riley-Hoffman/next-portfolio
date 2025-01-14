cba29bfcdbaf09735a4c6b4783811b431224a55b
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-14 10:55:41 -0500

    Added FaqSchema component to dynamically render SchemaInjector

---
+++ app/faq/components/accordion/schema/FaqSchema.tsx
@@ -0,0 +1,49 @@
+'use client'
+import dynamic from 'next/dynamic'
+import { FAQPage, WithContext } from 'schema-dts'
+import { questions, answers } from '../../accordion/constants/AccordionItems'
+import {
+  getBaseUrl,
+  getPageTitle,
+  author,
+  getImageUrl,
+} from '@/constants/baseData'
+
+export const description =
+  'Find the answers to my most frequently asked questions.'
+
+const SchemaInjector = dynamic(
+  () => import('@/app/components/schema/SchemaInjector'),
+  {
+    ssr: false,
+  }
+)
+const faqSchemaData: WithContext<FAQPage> = {
+  '@context': 'https://schema.org',
+  '@type': 'FAQPage',
+  name: getPageTitle('FAQ'),
+  description: description,
+  image: getImageUrl(),
+  url: `${getBaseUrl('/faq')}`,
+  datePublished: '2024-07-29T09:25:01.340Z',
+  mainEntity: questions.map((question, index) => ({
+    '@type': 'Question',
+    name: question,
+    acceptedAnswer: {
+      '@type': 'Answer',
+      text: `${answers[index].props.children}`,
+    },
+  })),
+  author: {
+    '@type': 'Person',
+    name: author,
+  },
+}
+
+export const FaqSchema = () => {
+  return (
+    <>
+      <SchemaInjector structuredData={faqSchemaData} />
+    </>
+  )
+}
--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,19 +1,12 @@
 import { Metadata } from 'next'
-import { FAQPage, WithContext } from 'schema-dts'
+import { FaqSchema } from './components/accordion/schema/FaqSchema'
 import { Accordion } from './components/accordion/Accordion'
 import {
   questions,
   answers,
 } from './components/accordion/constants/AccordionItems'
-import SchemaInjector from '../components/schema/SchemaInjector'
-import {
-  getBaseUrl,
-  getPageTitle,
-  author,
-  getImageUrl,
-} from '@/constants/baseData'
-
-const description = 'Find the answers to my most frequently asked questions.'
+import { description } from './components/accordion/schema/FaqSchema'
+import { getBaseUrl, getImageUrl } from '@/constants/baseData'
 
 export const metadata: Metadata = {
   title: 'FAQ',
@@ -30,32 +23,11 @@ export const metadata: Metadata = {
     url: `${getBaseUrl('/faq')}`,
   },
 }
-const faqSchemaData: WithContext<FAQPage> = {
-  '@context': 'https://schema.org',
-  '@type': 'FAQPage',
-  name: getPageTitle('FAQ'),
-  description: description,
-  image: getImageUrl(),
-  url: `${getBaseUrl('/faq')}`,
-  datePublished: '2024-07-29T09:25:01.340Z',
-  mainEntity: questions.map((question, index) => ({
-    '@type': 'Question',
-    name: question,
-    acceptedAnswer: {
-      '@type': 'Answer',
-      text: `${answers[index].props.children}`,
-    },
-  })),
-  author: {
-    '@type': 'Person',
-    name: author,
-  },
-}
 
 export default function Faq() {
   return (
     <>
-      <SchemaInjector structuredData={faqSchemaData} />
+      <FaqSchema />
       <h1 className="heading-one">Frequently Asked Questions</h1>
       <Accordion
         items={questions.map((question, index) => ({
