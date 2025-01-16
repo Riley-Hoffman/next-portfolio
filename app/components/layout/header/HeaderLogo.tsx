16bc8f66d45df9f066339e108510cc416056e848
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-16 12:28:09 -0500

    HeaderLogo: removed sr-only text. use aria-label instead.

--- app/components/layout/header/HeaderLogo.tsx
+++ app/components/layout/header/HeaderLogo.tsx
@@ -7,9 +7,10 @@ export const HeaderLogo = () => {
     <button
       className="m-0 pl-4 pr-0 text-center font-urbanist font-medium uppercase tracking-wider text-heading md:font-normal lg:text-2xl"
       onClick={() => router.push('/')}
+      aria-label="Riley Hoffman Back To Home Page"
+      role="link"
     >
       Riley Hoffman
-      <span className="sr-only">Back To Home Page</span>
     </button>
   )
 }
