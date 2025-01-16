720e03a486da7d39fd4b4ccd3370b18e6f3fc499
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-16 08:53:04 -0500

    CoverImage: removed redundant :before bg class

--- app/components/home/CoverImage.tsx
+++ app/components/home/CoverImage.tsx
@@ -21,7 +21,7 @@ export const CoverImage = ({ coverImageData }: CoverImageProps) => {
   const parallaxRef = useRef<HTMLDivElement>(null)
   return (
     <section
-      className="overlay gradient-border inverted overflow-hidden border-b-4 border-solid before:bg-[radial-gradient(rgba(255,255,255,0.8)_0%,_rgba(255,255,255,0.55)_100%),_linear-gradient(-30deg,_rgba(0,247,255,0.08)_0%,_#0000_15%,_#0000_80%,_rgba(0,247,255,0.08)_100%)]"
+      className="overlay gradient-border inverted overflow-hidden border-b-4 border-solid"
       ref={parallaxRef}
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
