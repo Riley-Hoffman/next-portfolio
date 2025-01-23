8354ac8a3c7fca57ba14f0767b775fffa94c17a1
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-23 12:24:31 -0500

    Revert dynamic header imports except ThemeChangerAndNav (ssr: false for js only).

--- app/components/layout/header/Header.tsx
+++ app/components/layout/header/Header.tsx
@@ -1,21 +1,22 @@
 'use client'
 import dynamic from 'next/dynamic'
+import { SkipToContent } from './SkipToContent'
+import { HeaderLogo } from './HeaderLogo'
+import { NoJsMenu } from './nav/NoJsMenu'
 import './styles/header.css'
 
-const ThemeChangerNav = dynamic(
+const ThemeChangerAndNav = dynamic(
   () => import('./dynamic-imports/ThemeChangerNav'),
   {
     ssr: false,
   }
 )
 
-const SkipToContentLogoNoJsMenu = dynamic(
-  () => import('./dynamic-imports/SkipToContentLogoNoJsMenu')
-)
-
 export const Header = () => (
   <header className="shadow[0_0.063rem_0.188rem_-0.188rem_black] border-b-2 border-t-[2rem] bg-[#f4eef6] dark:bg-[#0b1109]">
-    <SkipToContentLogoNoJsMenu />
-    <ThemeChangerNav />
+    <SkipToContent />
+    <HeaderLogo />
+    <ThemeChangerAndNav />
+    <NoJsMenu />
   </header>
 )
--- app/components/layout/header/dynamic-imports/ThemeChangerNav.tsx
+++ app/components/layout/header/dynamic-imports/ThemeChangerNav.tsx
@@ -2,10 +2,10 @@ import { ThemeChanger } from '../../../theme/ThemeChanger'
 import { Nav } from '../nav/Nav'
 
 const ThemeChangerNav = () => (
-  <>
-    <ThemeChanger />
-    <Nav />
-  </>
+    <>
+        <ThemeChanger />
+        <Nav />
+    </>
 )
 
 export default ThemeChangerNav
