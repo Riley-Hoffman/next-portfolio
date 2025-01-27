04ad537945ccd1a6c6daffa6cfc7e6de5a977eca
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-27 12:25:24 -0500

    eslint.config.mjs: removed @typescript-eslint/no-explicit-any rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -70,7 +70,6 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 
