53b6b0bfcf2c0408e18119282937d5a4098cc460
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-29 10:36:24 -0500

    useCreateParticle: formatting

--- app/projects/particle-cleanup/hooks/useCreateParticle.ts
+++ app/projects/particle-cleanup/hooks/useCreateParticle.ts
@@ -4,10 +4,10 @@ import { Particle } from '../classes/Particle'
 export const useCreateParticle = () =>
   useCallback((canvas: HTMLCanvasElement) => {
     const { width, height } = canvas
-    const size = Math.random() * 30 + 10
+    const size = 10 + Math.random() * 30
     const colors = ['#121226', '#21213b', '#363659', '#505077', '#6868a6']
     const color = colors[Math.floor(Math.random() * colors.length)]
-    const weight = Math.random() * 0.5 + 0.5
+    const weight = 0.5 + Math.random() * 0.5
     const isMobile = window.innerWidth <= 768
     const speedFactor = isMobile ? 0.48 : 0.6
     return new Particle(
