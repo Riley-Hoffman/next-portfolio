a9088d96ac187b82d147982fe77bf159ecb8e641
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-01-30 23:03:18 -0500

    Sidebar: moved long classlist to stylesheet

--- app/contact/components/sidebar/Sidebar.tsx
+++ app/contact/components/sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx'
 import { NewTabContent } from '@/app/components/utils/NewTabContent'
 import { sidebarLinks } from './constants/SidebarLinks'
+import './styles/sidebar.css'
 
 export const Sidebar = () => (
-  <aside className="mt-7 self-start bg-accentone-200 text-center md:sticky md:top-24 md:w-1/6 md:rounded-sm md:border-x md:border-b md:border-solid md:border-bordercolor">
+  <aside className="sidebar">
     <h3 className="mx-auto my-0 bg-heading text-accentone-200">Links</h3>
     {sidebarLinks.map(({ name, url }, index) => (
       <a
