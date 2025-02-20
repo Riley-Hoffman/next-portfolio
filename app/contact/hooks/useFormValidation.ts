e7088e2efeba7e8870ad22530b2706e522f97892
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-02-20 09:42:32 -0500

    useFormValidation: moved validateForm to a useCallback outside useEffect.

--- app/contact/hooks/useFormValidation.ts
+++ app/contact/hooks/useFormValidation.ts
@@ -1,39 +1,49 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { FormValue } from '../types/FormValue.interface'
 import isEmail from 'validator/lib/isEmail'
 
 export const useFormValidation = (initialState: FormValue) => {
   const [formState, setFormState] = useState(initialState)
   const [submitted, setSubmitted] = useState(false)
-  const [errors, setErrors] = useState({ name: '', email: '', message: '' })
+  const [errors, setErrors] = useState<Record<keyof FormValue, string>>({
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  const validateForm = useCallback(() => {
+    const { name, email, message } = formState
+    const newErrors = {
+      name: name.trim() === '' ? 'Please enter your name.' : '',
+      email: !isEmail(email) ? 'Please enter a valid email address.' : '',
+      message: message.trim() === '' ? 'Please enter a message.' : '',
+    }
+    setErrors(newErrors)
+  }, [formState])
+
   useEffect(() => {
     if (submitted) {
-      const validateForm = () => {
-        const { name, email, message } = formState
-        const newErrors = {
-          name: name.trim() === '' ? 'Please enter your name.' : '',
-          email: !isEmail(email) ? 'Please enter a valid email address.' : '',
-          message: message.trim() === '' ? 'Please enter a message.' : '',
-        }
-        setErrors(newErrors)
-      }
       validateForm()
     }
-  }, [formState, submitted])
+  }, [formState, submitted, validateForm])
 
   const handleChange =
     (field: keyof FormValue) =>
     (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { value } = event.target
+
       setFormState((prevState) => ({
         ...prevState,
-        [field]: event.target.value,
+        [field]: value,
       }))
+
       setSubmitted(false)
-      setErrors({ name: '', email: '', message: '' })
+      setErrors((prevErrors) => ({ ...prevErrors, [field]: '' }))
     }
 
   const handleUserSubmission = () => {
     setSubmitted(true)
+    validateForm()
   }
 
   return {
