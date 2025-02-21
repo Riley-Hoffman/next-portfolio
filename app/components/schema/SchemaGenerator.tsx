12e66899baeda43561ea369b5410ed6c9d873b33
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-02-21 08:42:38 -0500

    SchemaGenerator: added useMemo to cache schema

--- app/components/schema/SchemaGenerator.tsx
+++ app/components/schema/SchemaGenerator.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useMemo } from 'react'
 import dynamic from 'next/dynamic'
 import { WithContext, WebPage, ContactPage } from 'schema-dts'
 import {
@@ -25,48 +26,39 @@ type SchemaMap = {
   ContactPage: ContactPage
 }
 
-const SchemaInjector = dynamic(() => import('./SchemaInjector'), {
-  ssr: false,
+const SchemaInjector = dynamic(() => import('./SchemaInjector'), { ssr: false })
+
+const generateSchema = ({
+  schemaType,
+  title,
+  description,
+  urlPath,
+  publishDate,
+}: SchemaGeneratorProps['schemaData']): WithContext<
+  SchemaMap[typeof schemaType]
+> => ({
+  '@context': 'https://schema.org',
+  '@type': schemaType,
+  name: getPageTitle(title),
+  description,
+  image: getImageUrl(),
+  url: getBaseUrl(urlPath),
+  datePublished: publishDate,
+  mainEntity: {
+    '@type': 'Person',
+    name: author,
+    url: getBaseUrl(),
+    jobTitle: 'Web Developer',
+    sameAs: [linkedInUrl, githubUrl],
+  },
+  author: {
+    '@type': 'Person',
+    name: author,
+  },
 })
 
-const generateSchema = <T extends keyof SchemaMap>(
-  schemaType: T,
-  title: string,
-  description: string,
-  urlPath: string,
-  publishDate?: string
-): WithContext<SchemaMap[T]> => {
-  return {
-    '@context': 'https://schema.org',
-    '@type': schemaType,
-    name: getPageTitle(title),
-    description,
-    image: getImageUrl(),
-    url: getBaseUrl(urlPath),
-    datePublished: publishDate,
-    mainEntity: {
-      '@type': 'Person',
-      name: author,
-      url: getBaseUrl(),
-      jobTitle: 'Web Developer',
-      sameAs: [linkedInUrl, githubUrl],
-    },
-    author: {
-      '@type': 'Person',
-      name: author,
-    },
-  } as WithContext<SchemaMap[T]>
-}
-
 export const SchemaGenerator = ({ schemaData }: SchemaGeneratorProps) => {
-  const { title, description, urlPath, publishDate, schemaType } = schemaData
-  const schema = generateSchema(
-    schemaType,
-    title,
-    description,
-    urlPath,
-    publishDate
-  )
+  const schema = useMemo(() => generateSchema(schemaData), [schemaData])
 
   return <SchemaInjector structuredData={schema} />
 }
