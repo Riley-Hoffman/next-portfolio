fa8fbe341feaf5891a3a27ff71f576f919598ab9
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-02-22 08:38:59 -0500

    useThemeInfo: use only variables in return.

--- app/components/theme/hooks/useThemeInfo.ts
+++ app/components/theme/hooks/useThemeInfo.ts
@@ -2,14 +2,10 @@ import { useTheme } from 'next-themes'
 
 export const useThemeInfo = () => {
   const { theme, resolvedTheme, setTheme } = useTheme()
-
   const currentTheme = theme === 'system' ? resolvedTheme : theme
-
   const isDarkTheme = currentTheme === 'dark'
-
-  return {
-    icon: isDarkTheme ? 'solar:sun-2-bold' : 'solar:moon-bold',
-    label: isDarkTheme ? 'Switch to Light Theme' : 'Switch to Dark Theme',
-    toggleTheme: () => setTheme(isDarkTheme ? 'light' : 'dark'),
-  }
+  const icon = isDarkTheme ? 'solar:sun-2-bold' : 'solar:moon-bold'
+  const label = isDarkTheme ? 'Switch to Light Theme' : 'Switch to Dark Theme'
+  const toggleTheme = () => setTheme(isDarkTheme ? 'light' : 'dark')
+  return { icon, label, toggleTheme }
 }
