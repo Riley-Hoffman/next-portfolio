72828b570b4d7dcbefb77a46addef2b58f0313c1
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-02-22 08:52:41 -0500

    SkillsSection: simplified use of inset and padding class

--- app/skills/components/skills/SkillsSection.tsx
+++ app/skills/components/skills/SkillsSection.tsx
@@ -5,9 +5,9 @@ import '@/app/styles/background-accent.css'
 export const SkillsSection = () => (
   <section className="pb-16">
     <h1 className="heading-one">Skills</h1>
-    <div className="background-accent relative max-w-5xl after:inset-x-[5%] after:bottom-[3%] after:top-[5%] after:rounded-full">
+    <div className="background-accent relative max-w-5xl after:inset-12 after:rounded-full">
       <ul
-        className="mt-12 grid grid-cols-2 justify-items-center gap-x-6 gap-y-16 pb-12 pt-14 sm:grid-cols-3 md:grid-cols-4"
+        className="mt-12 grid grid-cols-2 justify-items-center gap-x-6 gap-y-16 py-14 sm:grid-cols-3 md:grid-cols-4"
         aria-label="Skills"
       >
         {skillsData.map((skill, index) => (
