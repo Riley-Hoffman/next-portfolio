106c4bc71e57b267d5c1c9b7d042f8bd86fe2397
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-03-01 08:37:35 -0500

    ProjectCategoryList: Map projectsByCat return outside main return.

--- app/components/projects/project-categories/ProjectCategoryList.tsx
+++ app/components/projects/project-categories/ProjectCategoryList.tsx
@@ -13,6 +13,35 @@ export const ProjectCategoryList = ({
   const projectsByCat = projects.filter(
     (project) => project.category === category
   )
+
+  const projectList = projectsByCat.map((project, index) => {
+    const projectNumber =
+      projects.findIndex((proj) => proj.title === project.title) + 1
+    const inverted = projectNumber % 2 === 0
+
+    const animation =
+      projectNumber > 1
+        ? inverted
+          ? 'motion-safe:md:left-[-200%] [&[data-active="true"]]:left-0'
+          : 'motion-safe:md:right-[-200%] [&[data-active="true"]]:right-0'
+        : ''
+
+    return (
+      <ProjectListItem
+        key={project.title}
+        inverted={inverted}
+        isFirst={index === 0}
+        animation={animation}
+      >
+        <ProjectContent
+          {...project}
+          inverted={inverted}
+          isFirst={index === 0}
+        />
+      </ProjectListItem>
+    )
+  })
+
   return (
     <section className="overflow-hidden first:pt-5 last:pb-20">
       <ProjectCatHeading category={category} />
@@ -20,30 +49,7 @@ export const ProjectCategoryList = ({
         className="max-w-screen-xl pb-5 text-base"
         aria-label={`${category} Projects`}
       >
-        {projectsByCat.map((project, catindex) => {
-          const projectNumber =
-            projects.findIndex((proj) => proj.title === project.title) + 1
-          const inverted = projectNumber % 2 === 0
-          let animation = ''
-          if (projectNumber > 1)
-            animation = inverted
-              ? 'motion-safe:md:left-[-200%] [&[data-active="true"]]:left-0'
-              : 'motion-safe:md:right-[-200%] [&[data-active="true"]]:right-0'
-          return (
-            <ProjectListItem
-              key={project.title}
-              inverted={inverted}
-              isFirst={project === projectsByCat[0]}
-              animation={animation}
-            >
-              <ProjectContent
-                {...project}
-                inverted={inverted}
-                isFirst={catindex === 0}
-              />
-            </ProjectListItem>
-          )
-        })}
+        {projectList}
       </ul>
     </section>
   )
