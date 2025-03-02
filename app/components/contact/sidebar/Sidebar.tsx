66224c15d820506b6700c52880bf2b47f15384cb
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-03-02 09:39:45 -0500

    Sidebar: simplify marging and padding.

--- app/components/contact/sidebar/Sidebar.tsx
+++ app/components/contact/sidebar/Sidebar.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx'
 import { NewTabContent } from '@/app/components/shared/NewTabContent'
 import { SIDEBAR_EXTERNAL_LINKS } from './constants/sidebarExternalLinks'
 import { EXTERNAL_LINK_ATTR } from '@/app/constants/externalLinkAttr'
@@ -6,14 +5,10 @@ import '@/app/styles/contact/sidebar.css'
 
 export const Sidebar = () => (
   <aside className="sidebar">
-    <h3 className="mx-auto my-0 bg-heading text-accentone-200">Links</h3>
-    {SIDEBAR_EXTERNAL_LINKS.map(({ name, url }, index) => (
+    <h3>Links</h3>
+    {SIDEBAR_EXTERNAL_LINKS.map(({ name, url }) => (
       <a
         key={name}
-        className={clsx(
-          'block font-urbanist no-underline',
-          index === 0 ? 'pb-2 pt-3' : 'pb-3 pt-2'
-        )}
         href={url}
         {...EXTERNAL_LINK_ATTR}
       >
