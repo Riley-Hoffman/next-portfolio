e89b083dcbfdd7b35a0625740584de1bbb25c58a
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-03-06 09:41:43 -0500

    useTriggerOnScroll: moved function defs out of useEffect. Debounced updateTriggerOnScroll.

--- app/hooks/shared/useTriggerOnScroll.ts
+++ app/hooks/shared/useTriggerOnScroll.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef, RefObject } from 'react'
 import { isBrowser } from '@/app/utils/isBrowser'
-
+import { useDebounce } from './useDebounce'
 type HTMLElementWithDataset = HTMLElement & {
   dataset: DOMStringMap & {
     active?: string
@@ -16,38 +16,38 @@ export const useTriggerOnScroll =
       return (rect.top < distance).toString()
     }
 
+    const updateElementActivation = (element: HTMLElementWithDataset): void => {
+      const rect = element.getBoundingClientRect()
+      const distance = parseInt(element.dataset.distance ?? '800', 10)
+      const newActiveState = getActiveState(rect, distance)
+
+      if (element.dataset.active !== newActiveState) {
+        element.dataset.active = newActiveState
+      }
+    }
+
+    const updateTriggerOnScroll = () => {
+      requestAnimationFrame(() => {
+        if (elementsRef.current) {
+          updateElementActivation(elementsRef.current)
+        }
+      })
+    }
+
+    const debouncedUpdateTriggerOnScroll = useDebounce(
+      updateTriggerOnScroll,
+      100
+    )
+
     useEffect(() => {
-      const distances = new Map<HTMLElementWithDataset, number>()
-
-      const updateElementActivation = (
-        element: HTMLElementWithDataset
-      ): void => {
-        const rect = element.getBoundingClientRect()
-        let distance = distances.get(element)
-        if (distance === undefined) {
-          distance = parseInt(element.dataset.distance ?? '800', 10)
-          distances.set(element, distance)
-        }
-        const newActiveState = getActiveState(rect, distance)
-
-        if (element.dataset.active !== newActiveState) {
-          element.dataset.active = newActiveState
-        }
-      }
-      const updateTriggerOnScroll = () => {
-        requestAnimationFrame(() => {
-          if (elementsRef.current) {
-            updateElementActivation(elementsRef.current)
-          }
-        })
-      }
-      updateTriggerOnScroll()
-
       if (isBrowser()) {
-        window.addEventListener('scroll', updateTriggerOnScroll)
-        return () => window.removeEventListener('scroll', updateTriggerOnScroll)
+        window.addEventListener('scroll', debouncedUpdateTriggerOnScroll)
+        debouncedUpdateTriggerOnScroll()
+        return () => {
+          window.removeEventListener('scroll', debouncedUpdateTriggerOnScroll)
+        }
       }
-    }, [])
+    }, [debouncedUpdateTriggerOnScroll])
 
     return elementsRef
   }
