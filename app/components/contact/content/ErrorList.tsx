2cbe196ab372d2f8e18f02314ba4d440b6c57c42
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-03-22 10:22:26 -0400

    ErrorList: formatting

--- app/components/contact/content/ErrorList.tsx
+++ app/components/contact/content/ErrorList.tsx
@@ -4,8 +4,8 @@ import '@/app/styles/shared/numbered-icons.css'
 export const ErrorList = ({ errors }: { errors: string[] }) => (
   <div className="pl-10">
     <Icon icon="bi:exclamation-circle-fill" className="mr-px size-9" />
-    <h2 className="inline-block align-middle pl-2">Error processing form</h2>
-    <ol className="numbered-icons pb-6 px-5 text-xl">
+    <h2 className="inline-block pl-2 align-middle">Error processing form</h2>
+    <ol className="numbered-icons px-5 pb-6 text-xl">
       {errors.map((error) => (
         <li key={error} className="before:text-heading">
           {error}
