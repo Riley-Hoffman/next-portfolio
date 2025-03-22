3013157742d9146ec1ed11bb13d82fe3d2d6afb3
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-03-22 09:45:40 -0400

    MyJourney: fix border variables to full classNames

--- app/components/home/my-journey/MyJourney.tsx
+++ app/components/home/my-journey/MyJourney.tsx
@@ -9,8 +9,8 @@ const LazyHueRotateOnScroll = dynamic(
 
 export const MyJourney = () => {
   const hueElRef = useRef<HTMLElement>(null)
-  const LIGHT_BORDER_COLOR = '[#d6d2ee]'
-  const DARK_BORDER_COLOR = '[#292d11]'
+  const LIGHT_BORDER = 'border-[#d6d2ee]'
+  const DARK_BORDER = 'dark:border-[#292d11]'
 
   return (
     <section
@@ -19,7 +19,7 @@ export const MyJourney = () => {
     >
       <figure itemScope itemType="https://schema.org/ImageObject">
         <Image
-          className={`size-96 max-w-full rounded-3xl object-cover border-${LIGHT_BORDER_COLOR} dark:border-${DARK_BORDER_COLOR} border`}
+          className={`size-96 max-w-full rounded-3xl object-cover border ${LIGHT_BORDER} ${DARK_BORDER}`}
           src="https://storage.googleapis.com/rileyhoffmandotcom.appspot.com/headshot.webp"
           width={384}
           height={452}
@@ -31,16 +31,14 @@ export const MyJourney = () => {
           Photo by Kristen Mommertz
         </figcaption>
       </figure>
-      <div
-        className={`m-6 border-2 border-${LIGHT_BORDER_COLOR} bg-[whitesmoke] py-6 leading-7 contrast-more:bg-white dark:border-${DARK_BORDER_COLOR} dark:bg-siteblack md:w-2/3 lg:px-[8%]`}
-      >
+      <div className={`m-6 border-2 ${LIGHT_BORDER} bg-[whitesmoke] py-6 leading-7 contrast-more:bg-white ${DARK_BORDER} dark:bg-siteblack md:w-2/3 lg:px-[8%]`}>
         <h2>My Journey</h2>
         <p>
-          My career journey began in customer service, tech support, and various
-          non-profit organizations. Although these roles were valuable, I was
-          eager to find a path that would truly ignite my passion and offer more
-          engaging skills. This pursuit led me to web development through one of
-          these non-profits.
+          My career journey began in customer service, tech support, and
+          various non-profit organizations. Although these roles were
+          valuable, I was eager to find a path that would truly ignite my
+          passion and offer more engaging skills. This pursuit led me to web
+          development through one of these non-profits.
         </p>
         <p>
           Captivated by the potential for creativity and constant learning, I
@@ -49,9 +47,9 @@ export const MyJourney = () => {
         </p>
         <p>
           I have professional experience as a web developer, building and
-          maintaining dynamic, user-friendly websites. I’m now excited to bring
-          my skills and passion to new opportunities, eager to contribute to
-          innovative projects and grow further in the field.
+          maintaining dynamic, user-friendly websites. I’m now excited to
+          bring my skills and passion to new opportunities, eager to
+          contribute to innovative projects and grow further in the field.
         </p>
         <p>
           I believe that by prioritizing accessibility in our work as web
