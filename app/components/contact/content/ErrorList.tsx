da4e76546dc7833f1263fdd22a224bad32849dc2
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-03-22 10:21:57 -0400

    ErrorList: simplify html/tailwind classes

--- app/components/contact/content/ErrorList.tsx
+++ app/components/contact/content/ErrorList.tsx
@@ -2,17 +2,15 @@ import { Icon } from '@iconify/react'
 import '@/app/styles/shared/numbered-icons.css'
 
 export const ErrorList = ({ errors }: { errors: string[] }) => (
-  <>
-    <div className="flex items-center pl-5">
-      <Icon icon="bi:exclamation-circle-fill" className="ml-5 mr-px size-9" />
-      <h2 className="pl-2">Error processing form</h2>
-    </div>
-    <ol className="numbered-icons pb-6 pl-16 pr-5 text-xl">
+  <div className="pl-10">
+    <Icon icon="bi:exclamation-circle-fill" className="mr-px size-9" />
+    <h2 className="inline-block align-middle pl-2">Error processing form</h2>
+    <ol className="numbered-icons pb-6 px-5 text-xl">
       {errors.map((error) => (
         <li key={error} className="before:text-heading">
           {error}
         </li>
       ))}
     </ol>
-  </>
+  </div>
 )
