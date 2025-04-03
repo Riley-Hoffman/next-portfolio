6c0272085a51dd6ff56de63093915b87535fd9d7
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-04-03 09:49:28 -0400

    TrainingSection: remove unused ID

--- app/components/skills/trainings/TrainingsSection.tsx
+++ app/components/skills/trainings/TrainingsSection.tsx
@@ -3,9 +3,7 @@ import { SLIDE_DATA } from '@/app/constants/carousel/slideData'
 
 export const TrainingsSection = () => (
   <section className="gradient-border border-t-2 bg-accentone-100 pb-16 pt-7">
-    <h2 className="max-w-fit pb-5" id="trainingsLabel">
-      Trainings & Certifications
-    </h2>
+    <h2 className="max-w-fit pb-5">Trainings & Certifications</h2>
     <Carousel slides={SLIDE_DATA} />
   </section>
 )
