8af32e69fcd174d5f7ddb47245575e7964361d85
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-06-13 19:33:23 -0400

    Add hovershadow color var apply to hover shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
       colors: {
         heading: 'var(--color-heading)',
         text: 'var(--color-text)',
+        hovershadow: 'var(--color-hovershadow)',
         accentone: {
           100: 'var(--color-accentone-100)',
           200: 'var(--color-accentone-200)',
