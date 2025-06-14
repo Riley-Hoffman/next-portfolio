299f37cab86cde9bab3d20eb20d59a9f849b83e1
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-06-14 18:02:37 -0400

    metadata: use standard string for og title instead of template

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import { inconsolata, poppins, sourcesans, urbanist } from './fonts/fonts'
 import { DynamicThemeProvider } from './components/theme/DynamicThemeProvider'
 import { LayoutContent } from './components/layout/LayoutContent'
-import { IconLinks } from './components/layout/IconLinks'
-import { ICON_LINK_ATTRIBUTES } from './constants/icon-links/iconLinkAttributes'
+// import { IconLinks } from './components/layout/IconLinks'
+// import { ICON_LINK_ATTRIBUTES } from './constants/icon-links/iconLinkAttributes'
 import { getBaseUrl, getImageUrl, getPageTitle, BASE_DESCRIPTION } from '@/app/constants/baseData'
 import './styles/globals.css'
 
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     template: '%s - Riley Hoffman - Web Developer',
   },
   description: BASE_DESCRIPTION,
-  metadataBase: new URL(`${getBaseUrl()}`),
+  metadataBase: new URL(getBaseUrl()),
   alternates: {
     canonical: '/',
   },
@@ -21,17 +21,18 @@ export const metadata: Metadata = {
     type: 'website',
     locale: 'en_CA',
     images: getImageUrl(),
-    title: {
-      template: `%s - ${getPageTitle()}`,
-      default: `${getPageTitle()}`,
-    },
+    title: getPageTitle(),
     url: getBaseUrl(),
   },
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#eee3f2' },
+    { media: '(prefers-color-scheme: dark)', color: '#0d191c' },
+  ]
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
-    <head>
+    {/* <head>
       {ICON_LINK_ATTRIBUTES.map((icon, index) => (
         <IconLinks key={index} {...icon} />
       ))}
@@ -45,7 +46,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
         media="(prefers-color-scheme: dark)"
         content="#0d191c"
       />
-    </head>
+    </head> */}
     <body
       className={`${inconsolata.variable} ${poppins.variable} ${sourcesans.variable} ${urbanist.variable} antialiased contrast-more:subpixel-antialiased`}
     >
--- app/utils/metadata.ts
+++ app/utils/metadata.ts
@@ -11,22 +11,24 @@ export const createMetadata = ({
   title,
   description,
   path,
-}: PageMetadataProps): Metadata => ({
-  title,
-  description,
-  metadataBase: new URL(getBaseUrl(path)),
-  alternates: {
-    canonical: '/',
-  },
-  openGraph: {
-    type: 'website',
-    locale: 'en_CA',
-    images: getImageUrl(),
-    title,
-    url: getBaseUrl(path),
-  },
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#eee3f2' },
-    { media: '(prefers-color-scheme: dark)', color: '#0d191c' },
-  ],
-})
+}: PageMetadataProps): Metadata => {
+  return {
+    title: title,
+    description,
+    metadataBase: new URL(getBaseUrl(path)),
+    alternates: {
+      canonical: '/',
+    },
+    openGraph: {
+      type: 'website',
+      locale: 'en_CA',
+      images: getImageUrl(),
+      title: `${title} - Riley Hoffman - Web Developer`,
+      url: getBaseUrl(path),
+    },
+    themeColor: [
+      { media: '(prefers-color-scheme: light)', color: '#eee3f2' },
+      { media: '(prefers-color-scheme: dark)', color: '#0d191c' },
+    ],
+  }
+}
