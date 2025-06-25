0fd75c0988c99d4551429106383dca89eba42d68
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-06-25 09:24:30 -0400

    technologies.ts: add variable for packageJson.dependencies to simply version paths.

--- app/constants/home/technologies.ts
+++ app/constants/home/technologies.ts
@@ -1,35 +1,37 @@
 import packageJson from '@/package.json'
 import { Tech } from '@/app/types/home/Tech.interface'
 
+const dependencies = packageJson.dependencies
+
 export const TECHNOLOGIES: Tech[] = [
   {
     name: 'React',
     logo: 'cib:react',
     url: 'https://react.dev',
-    version: packageJson.dependencies.react,
+    version: dependencies.react,
   },
   {
     name: 'Next.js',
     logo: 'devicon-plain:nextjs',
     url: 'https://nextjs.org',
-    version: packageJson.dependencies.next,
+    version: dependencies.next,
   },
   {
     name: 'TypeScript',
     logo: 'cib:typescript',
     url: 'https://www.typescriptlang.org',
-    version: packageJson.dependencies.typescript,
+    version: dependencies.typescript,
   },
   {
     name: 'Tailwind CSS',
     logo: 'bxl:tailwind-css',
     url: 'https://tailwindcss.com',
-    version: packageJson.dependencies.tailwindcss,
+    version: dependencies.tailwindcss,
   },
   {
     name: 'Firebase',
     logo: 'bxl:firebase',
     url: 'https://firebase.google.com',
-    version: packageJson.dependencies.firebase,
+    version: dependencies.firebase,
   },
 ]
