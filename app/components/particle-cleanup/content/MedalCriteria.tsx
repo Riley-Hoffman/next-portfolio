54eeeb6a1ae67ca3fcc2ecaf68c3ab7030865d4a
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-07-02 12:56:32 -0400

    MedalCriteria: formatting

--- app/components/particle-cleanup/content/MedalCriteria.tsx
+++ app/components/particle-cleanup/content/MedalCriteria.tsx
@@ -9,9 +9,7 @@ export const MedalCriteria = ({
 }: MedalCriteriaData) => (
   <li className="mr-9">
     <Icon className="mr-3 text-4xl" icon="fa6-solid:medal" color={color} />
-    <span className="sr-only">
-      {`${name}, ${srText}.`}
-    </span>
+    <span className="sr-only">{`${name}, ${srText}.`}</span>
     <span aria-hidden={true}>{time}</span>
   </li>
 )
