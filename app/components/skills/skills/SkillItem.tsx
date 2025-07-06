c86e5cfe1a0a0631a60e519255e950631f687e3f
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-07-06 13:01:01 -0400

    Skillitem.tsx: add div with min-h to contain icons.

--- app/components/skills/skills/SkillItem.tsx
+++ app/components/skills/skills/SkillItem.tsx
@@ -3,8 +3,12 @@ import { Icon } from '@iconify/react'
 import { Skill } from '@/app/types/skills/Skill.interface'
 
 export const SkillItem = ({ skill, icon, classes }: Skill) => (
-  <li className="min-h-11 text-center">
-    {icon && <Icon className={clsx('text-5xl', classes)} icon={icon} />}
-    <p className="mt-1 text-xl">{skill}</p>
+  <li className="min-h-24 text-center">
+    {icon && (
+      <div className="min-h-12">
+        <Icon className={clsx('text-5xl', classes)} icon={icon} />
+      </div>
+    )}
+    <p className="mb-4 mt-1 text-xl">{skill}</p>
   </li>
 )
