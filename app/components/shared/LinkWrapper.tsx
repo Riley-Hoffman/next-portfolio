bd4c34465f5eb62a59c8f017dca69966ae8ebc77
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-07-26 11:09:14 -0400

    LinkWrapper: remove unnecessary fragment.

--- app/components/shared/LinkWrapper.tsx
+++ app/components/shared/LinkWrapper.tsx
@@ -3,49 +3,47 @@ import { NewTabContent } from '@/app/components/shared/NewTabContent'
 import { NewTabContentProps } from '@/app/types/new-tab-content/NewTabContent'
 
 interface LinkWrapperProps
-  extends NewTabContentProps,
+    extends NewTabContentProps,
     React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  href: string
-  children: React.ReactNode
+    href: string
+    children: React.ReactNode
 }
 
 const INTERNAL_DOMAINS = ['https://rileyhoffman.com', 'http://localhost']
 
 const isExternal = (href: string): boolean => {
-  if (href.startsWith('/')) return false
-  return !INTERNAL_DOMAINS.some((domain) => href.startsWith(domain))
+    if (href.startsWith('/')) return false
+    return !INTERNAL_DOMAINS.some((domain) => href.startsWith(domain))
 }
 
 export const LinkWrapper = ({
-  href,
-  children,
-  showNewTabIcon = false,
-  hideIconOnMobile = false,
-  ...rest
+    href,
+    children,
+    showNewTabIcon = false,
+    hideIconOnMobile = false,
+    ...rest
 }: LinkWrapperProps) => {
-  if (isExternal(href)) {
-    return (
-      <a href={href} target="_blank" rel="noopener noreferrer" {...rest}>
-        <>
-          {children}
-          <NewTabContent
-            hideIconOnMobile={hideIconOnMobile}
-            showNewTabIcon={showNewTabIcon}
-          />
-        </>
-      </a>
+    if (isExternal(href)) {
+        return (
+            <a href={href} target="_blank" rel="noopener noreferrer" {...rest}>
+                {children}
+                <NewTabContent
+                    hideIconOnMobile={hideIconOnMobile}
+                    showNewTabIcon={showNewTabIcon}
+                />
+            </a>
+        )
+    }
+
+    const normalizedHref = INTERNAL_DOMAINS.find((domain) =>
+        href.startsWith(domain)
     )
-  }
+        ? href.replace(/^https?:\/\/[^/]+/, '')
+        : href
 
-  const normalizedHref = INTERNAL_DOMAINS.find((domain) =>
-    href.startsWith(domain)
-  )
-    ? href.replace(/^https?:\/\/[^/]+/, '')
-    : href
-
-  return (
-    <Link href={normalizedHref} {...rest}>
-      {children}
-    </Link>
-  )
+    return (
+        <Link href={normalizedHref} {...rest}>
+            {children}
+        </Link>
+    )
 }
