85d031bed56b84170c9f00998d2199514cf6eec4
Riley Hoffman <riley.n.hoffman@gmail.com>
2025-09-19 13:39:10 -0400

    Update theme changer aria-label

--- app/hooks/theme/useThemeInfo.ts
+++ app/hooks/theme/useThemeInfo.ts
@@ -5,7 +5,7 @@ export const useThemeInfo = () => {
   const currentTheme = theme === 'system' ? resolvedTheme : theme
   const isDarkTheme = currentTheme === 'dark'
   const icon = isDarkTheme ? 'solar:sun-2-bold' : 'solar:moon-bold'
-  const label = isDarkTheme ? 'Switch to Light Theme' : 'Switch to Dark Theme'
+  const label = isDarkTheme ? 'Light Theme' : 'Dark Theme'
   const toggleTheme = () => setTheme(isDarkTheme ? 'light' : 'dark')
 
   return { icon, label, toggleTheme, isDarkTheme }
